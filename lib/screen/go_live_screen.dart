f1dad3ab25f9cc14c1517814a3ed0c2c64c0d7eb
Zufaa <riyazurrohmankawchar@gmail.com>
2022-12-31 13:29:26 +0600

    go live ui design 1

--- lib/screen/go_live_screen.dart
+++ lib/screen/go_live_screen.dart
@@ -1,4 +1,6 @@
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
+import 'package:live_streaming_pro_2/utils/colors.dart';
 
 class GoLiveScreen extends StatefulWidget {
   const GoLiveScreen({Key? key}) : super(key: key);
@@ -11,9 +13,31 @@ class _GoLiveScreenState extends State<GoLiveScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Go live screen"),
-      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Column(
+              children: [
+                DottedBorder(
+                  borderType: BorderType.RRect,
+                    radius:const Radius.circular(10),
+                    dashPattern:const [10,4],
+                    strokeCap: StrokeCap.round,
+                    color: buttonColor,
+                    child: Container(
+                      width: double.infinity,
+                      height: 150,
+                      decoration: BoxDecoration(
+                        color: buttonColor.withOpacity(.05),
+                        borderRadius: BorderRadius.circular(10)
+                      ),
+                    )
+                )
+              ],
+            )
+          ],
+        ),
+      )
     );
   }
 }
