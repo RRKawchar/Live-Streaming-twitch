3ae0f0a587525d8730f4ba8d204508afaf32d60e
Zufaa <riyazurrohmankawchar@gmail.com>
2022-12-31 13:14:49 +0600

    home screen ui design

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:live_streaming_pro_2/provider/user_provider.dart';
+import 'package:live_streaming_pro_2/screen/feed_screen.dart';
+import 'package:live_streaming_pro_2/screen/go_live_screen.dart';
+import 'package:live_streaming_pro_2/utils/colors.dart';
 import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -11,15 +14,37 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<Widget> pages=[
+    const FeedScreen(),
+    const GoLiveScreen(),
+    const Center(child: Text("Browser"),)
+  ];
+
+  int _currentPage = 0;
+  void onChangePage(int page) {
+    _currentPage = page;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    final userProvider = Provider.of<UserProvider>(context);
     return Scaffold(
-      body: Center(
-        child: Text(
-          userProvider.users.username,style: const TextStyle(color: Colors.black),
-        ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: buttonColor,
+        unselectedItemColor: primaryColor,
+        backgroundColor: backgroundColor,
+        unselectedFontSize: 12,
+        onTap: onChangePage,
+        currentIndex: _currentPage,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite), label: "Following"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.add_rounded), label: "Go Live"),
+          BottomNavigationBarItem(icon: Icon(Icons.copy), label: "Browser"),
+        ],
       ),
+      body: pages[_currentPage],
     );
   }
 }
