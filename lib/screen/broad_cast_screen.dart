b78c34f6dbc0d3be31d3dd1e571b3439b5ad249e
Zufaa <riyazurrohmankawchar@gmail.com>
2023-01-02 11:27:35 +0600

    agora connection function in broad cast

--- lib/screen/broad_cast_screen.dart
+++ lib/screen/broad_cast_screen.dart
@@ -1,17 +1,100 @@
+import 'package:agora_rtc_engine/agora_rtc_engine.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:live_streaming_pro_2/config/app_id.dart';
+import 'package:live_streaming_pro_2/provider/user_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:provider/provider.dart';
+import 'package:agora_rtc_engine/agora_rtc_engine.dart' as RtcLocalView;
 
 class BroadCastScreen extends StatefulWidget {
-  const BroadCastScreen({Key? key}) : super(key: key);
+  final bool isBroadCaster;
+  final String channelId;
+  const BroadCastScreen(
+      {Key? key, required this.isBroadCaster, required this.channelId})
+      : super(key: key);
 
   @override
   State<BroadCastScreen> createState() => _BroadCastScreenState();
 }
 
 class _BroadCastScreenState extends State<BroadCastScreen> {
+  late final RtcEngine _engine;
+  List<int> remoteUid = [];
+  @override
+  void initState() {
+    _initEngine();
+    super.initState();
+  }
+
+  void _initEngine() async {
+    _engine = createAgoraRtcEngine();
+    await _engine.initialize(RtcEngineContext(
+      appId: appId,
+    ));
+
+    _addListeners();
+    await _engine.enableVideo();
+    await _engine.startPreview();
+    await _engine
+        .setChannelProfile(ChannelProfileType.channelProfileLiveBroadcasting);
+    if (widget.isBroadCaster) {
+      _engine.setClientRole(role: ClientRoleType.clientRoleBroadcaster);
+    } else {
+      _engine.setClientRole(role: ClientRoleType.clientRoleAudience);
+    }
+    _joinChannel();
+  }
+
+  void _addListeners() {
+    _engine.registerEventHandler(RtcEngineEventHandler(
+        onJoinChannelSuccess: (RtcConnection connection, int elapsed) {
+      debugPrint('onJoinChannelSuccess $connection, $elapsed');
+    }, onUserJoined: (RtcConnection connection, int uid, int elapsed) {
+      debugPrint('onUserJoined $connection $uid $elapsed');
+      remoteUid.add(uid);
+    }, onUserOffline: (RtcConnection connection, uid, reason) {
+      debugPrint('onUserOffline $connection $uid $reason');
+
+      setState(() {
+        remoteUid.removeWhere((element) => element == uid);
+      });
+    }, onLeaveChannel: (RtcConnection connection, stats) {
+      debugPrint("onLeaveChannel $stats");
+      setState(() {
+        remoteUid.clear();
+      });
+    }));
+  }
+
+  void _joinChannel() async {
+    if (defaultTargetPlatform == TargetPlatform.android) {
+      await [Permission.microphone, Permission.camera].request();
+    }
+    await _engine.joinChannelWithUserAccount(
+      token: tempToken,
+      channelId: "rrkkas",
+      userAccount: Provider.of<UserProvider>(context, listen: false).users.uid,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+
+    final users=Provider.of<UserProvider>(context).users;
+    return Scaffold(
+      body: Padding(
+        padding:const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            _renderVideo(users)
+          ],
+        ),
+      ),
+    );
+  }
+  _renderVideo(users){
+    return AspectRatio(aspectRatio: 16/9,
 
     );
   }
--- lib/screen/go_live_screen.dart
+++ lib/screen/go_live_screen.dart
@@ -31,11 +31,16 @@ class _GoLiveScreenState extends State<GoLiveScreen> {
     String channelId = await FirestoreMethods()
         .startLiveStream(context, _titleController.text, image);
     if (channelId.isNotEmpty) {
+      // ignore: use_build_context_synchronously
       showSnakBar(context, "livestream has started successfully!");
+      // ignore: use_build_context_synchronously
       Navigator.push(
         context,
         MaterialPageRoute(
-          builder: (context) => BroadCastScreen(),
+          builder: (context) => BroadCastScreen(
+            isBroadCaster: true,
+            channelId: channelId,
+          ),
         ),
       );
     }
@@ -45,86 +50,88 @@ class _GoLiveScreenState extends State<GoLiveScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 18.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              children: [
-                InkWell(
-                  onTap: () async {
-                    Uint8List? pickedImage = await pickImage();
-                    if (pickedImage != null) {
-                      setState(() {
-                        image = pickedImage;
-                      });
-                    }
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 22.0, vertical: 20.0),
-                    child: image != null
-                        ? SizedBox(
-                            height: 300,
-                            child: Image.memory(image!),
-                          )
-                        : DottedBorder(
-                            borderType: BorderType.RRect,
-                            radius: const Radius.circular(10),
-                            dashPattern: const [10, 4],
-                            strokeCap: StrokeCap.round,
-                            color: buttonColor,
-                            child: Container(
-                              width: double.infinity,
-                              height: 150,
-                              decoration: BoxDecoration(
-                                  color: buttonColor.withOpacity(.05),
-                                  borderRadius: BorderRadius.circular(10)),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  const Icon(
-                                    Icons.folder_open,
-                                    color: buttonColor,
-                                    size: 40.0,
-                                  ),
-                                  const SizedBox(
-                                    height: 15,
-                                  ),
-                                  Text(
-                                    "Select your thumbnail",
-                                    style: TextStyle(
-                                      fontSize: 15,
-                                      color: Colors.grey.shade400,
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 18.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  InkWell(
+                    onTap: () async {
+                      Uint8List? pickedImage = await pickImage();
+                      if (pickedImage != null) {
+                        setState(() {
+                          image = pickedImage;
+                        });
+                      }
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 22.0, vertical: 20.0),
+                      child: image != null
+                          ? SizedBox(
+                              height: 300,
+                              child: Image.memory(image!),
+                            )
+                          : DottedBorder(
+                              borderType: BorderType.RRect,
+                              radius: const Radius.circular(10),
+                              dashPattern: const [10, 4],
+                              strokeCap: StrokeCap.round,
+                              color: buttonColor,
+                              child: Container(
+                                width: double.infinity,
+                                height: 150,
+                                decoration: BoxDecoration(
+                                    color: buttonColor.withOpacity(.05),
+                                    borderRadius: BorderRadius.circular(10)),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    const Icon(
+                                      Icons.folder_open,
+                                      color: buttonColor,
+                                      size: 40.0,
                                     ),
-                                  )
-                                ],
+                                    const SizedBox(
+                                      height: 15,
+                                    ),
+                                    Text(
+                                      "Select your thumbnail",
+                                      style: TextStyle(
+                                        fontSize: 15,
+                                        color: Colors.grey.shade400,
+                                      ),
+                                    )
+                                  ],
+                                ),
                               ),
                             ),
-                          ),
-                  ),
-                ),
-                const SizedBox(height: 10),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Title",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
                     ),
-                    CustomTextField(controller: _titleController)
-                  ],
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 10),
-              child: Custombutton(onTap: goLiveStream, text: "Go Live!"),
-            )
-          ],
+                  ),
+                  const SizedBox(height: 10),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Title",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      CustomTextField(controller: _titleController)
+                    ],
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 10),
+                child: Custombutton(onTap: goLiveStream, text: "Go Live!"),
+              )
+            ],
+          ),
         ),
       ),
     ));
